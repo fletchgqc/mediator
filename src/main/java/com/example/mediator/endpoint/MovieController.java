8db2e9b8f6e402d499c5c802630855af0ec36dbe
Manuel <manuel.wessner@codecentric.de>
2020-01-14 13:51:05 +0100

    Remove unneeded import

--- src/main/java/com/example/mediator/endpoint/MovieController.java
+++ src/main/java/com/example/mediator/endpoint/MovieController.java
@@ -1,10 +1,9 @@
 package com.example.mediator.endpoint;
 
 import com.example.mediator.domain.Movie;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.mediator.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.mediator.service.MovieService;
 
 @RestController
 public class MovieController {
